73e65efaba8f83814af667574b099e735c413bf8
Evan Mattiza <emattiza@gmail.com>
2022-12-08 16:47:39 +0000

    113 done

--- koans/py/113-easy-builder-pattern.py
+++ koans/py/113-easy-builder-pattern.py
@@ -4,27 +4,28 @@ Koan to learn annotating the self instance
 # This is called builder pattern or fluent interface. You can read about in the following posts
 # https://kracekumar.com/post/100897281440/fluent-interface-in-python/
 # https://python-patterns.guide/gang-of-four/builder/
+from typing import Type
 
 
 class Poem:
-    def __init__(self, content):
+    def __init__(self, content: str) -> None:
         self.content = content
 
     # Annotate the method to return the instance of Poem class
-    def _clone(self, content):
+    def _clone(self, content: str) -> "Poem":
         return Poem(content=content)
 
     # Annotate the method to return the instance of Poem class
-    def indent(self, spaces=4):
+    def indent(self, spaces: int=4) -> "Poem":
         content = " " * spaces + self.content
         return self._clone(content)
 
     # Annotate the method to return the instance of Poem class
-    def suffix(self, content):
+    def suffix(self, content: str) -> "Poem":
         content = self.content + " - {}".format(content)
         return self._clone(content)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.content
 
 
