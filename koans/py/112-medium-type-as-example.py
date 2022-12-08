42f2f6a27edd500f3f42e4bb6a6b2e9475a227a7
Evan Mattiza <emattiza@gmail.com>
2022-12-08 16:35:39 +0000

    112 done

--- koans/py/112-medium-type-as-example.py
+++ koans/py/112-medium-type-as-example.py
@@ -3,10 +3,10 @@ Koan to learn type of class when passed as an argument.
 """
 
 import json
-from typing import Any, Type
+from typing import Any, Dict, Type
 
 # Figure out how to ignore Any Error
-JSONData = dict[str, Any]
+JSONData = Dict[str, Any] # type: ignore
 
 
 class JSONResponse:
@@ -14,13 +14,13 @@ class JSONResponse:
         self.data = data
 
     # Annotate the return type
-    def make_response(self):
+    def make_response(self) -> bytes:
         return json.dumps(self.data).encode("utf-8")
 
 
 # Annotate the return type as the type of response class
 # Documentation: https://docs.python.org/3/library/typing.html?highlight=typing#typing.Type
-def make_response(resp_klass, data):
+def make_response(resp_klass: Type[JSONResponse], data: JSONData) -> bytes:
     return resp_klass(data).make_response()
 
 
